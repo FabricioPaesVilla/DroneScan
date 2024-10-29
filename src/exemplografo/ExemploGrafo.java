f2fd0c6fb418ee3bb6f7f45014a8243107f59b9a
GIOVANNA DIAS DE ALMEIDA MUNTE <giovanna.damunte@senacsp.edu.br>
2024-10-29 11:07:54 -0300

    Atualiza Grafo sem busca v2.0

--- src/exemplografo/ExemploGrafo.java
+++ src/exemplografo/ExemploGrafo.java
@@ -1,2 +1,19 @@
-package exemplografo;public class ExemploGrafo {
+package exemplografo;
+
+public class ExemploGrafo {
+    public static void main(String[] args) {
+        Grafo<String> grafo = new Grafo<String>();
+        grafo.adicionarVertice("Paulo");
+        grafo.adicionarVertice("Lorenzo");
+        grafo.adicionarVertice("Cleuza");
+        grafo.adicionarVertice("Kleber");
+        grafo.adicionarVertice("Claudio");
+
+        grafo.adicionaAresta(2.0,"Paulo","Lorenzo");
+        grafo.adicionaAresta(3.0,"Lorenzo","Kleber");
+        grafo.adicionaAresta(1.0,"Kleber","Cleuza");
+        grafo.adicionaAresta(1.0,"Paulo","Cleuza");
+        grafo.adicionaAresta(2.0,"Claudio","Lorenzo");
+        grafo.adicionaAresta(3.0,"Claudio","Paulo");
+    }
 }
--- src/exemplografo/Grafo.java
+++ src/exemplografo/Grafo.java
@@ -1,2 +1,41 @@
-package exemplografo;public class Grafo {
+package exemplografo;
+
+import java.util.ArrayList;
+
+public class Grafo<TIPO> {
+    private ArrayList<Vertice<TIPO>> vertices;
+    private ArrayList<Aresta<TIPO>> aresta;
+
+    public Grafo() {
+        this.vertices = new ArrayList<Vertice<TIPO>>();
+        this.aresta = new ArrayList<Aresta<TIPO>>();
+    }
+    public void adicionarVertice(TIPO dado) {
+        Vertice<TIPO> novoVertice = new Vertice<TIPO>(dado);
+        this.vertices.add(novoVertice);
+    }
+
+    public void adicionaAresta(Double peso, TIPO dadoInicio, TIPO dadoFim){
+        Vertice<TIPO> inicio = this.getVertice(dadoInicio);
+        Vertice<TIPO> fim = this.getVertice(dadoFim);
+        Aresta<TIPO> aresta = new Aresta<TIPO>(peso, inicio, fim);
+        inicio.adicionaArestaSaida(aresta);
+        fim.adicionaArestaEntrada(aresta);
+        this.aresta.add(aresta);
+    }
+
+    public Vertice<TIPO> getVertice(TIPO dado){
+        Vertice<TIPO> vertice = null;
+        for (int i = 0; i < this.vertices.size(); i++) {
+            if (this.vertices.get(i).getDado().equals(dado)){
+                vertice = this.vertices.get(i);
+                break;
+            }
+        }
+        return vertice;
+    }
+
+    public String buscaCaminho(TIPO de, TIPO para){
+
+    }
 }
